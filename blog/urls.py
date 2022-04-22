1ff3124f365fc73ea2f3e74eab4799bee077c019
codeshubham <shubhammank@gmail.com>
2022-04-22 20:57:20 +0530

    jwt

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path,include 
-from django.contrib import admin
+from django.urls import path
+from django.views.generic import TemplateView
+
 app_name = 'blog'
 
 urlpatterns = [
-    
+    path('', TemplateView.as_view(template_name="blog/index.html")),
 ]
